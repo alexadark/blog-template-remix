4435283b960b0057d9fffe1b038b549c871e56b2
Alexa spalato <alexaspalato@gmail.com>
2023-10-18 09:02:43 +0200

    config correction

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,6 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
   // serverModuleFormat: "cjs",
   // serverDependenciesToBundle: ["react-syntax-highlighter"],
 };
