b2284a9c08cd6810765c4983418f434069442021
Alexa spalato <alexaspalato@gmail.com>
2023-10-18 15:39:12 +0200

    tag

--- app/components/bloks/Tag.tsx
+++ app/components/bloks/Tag.tsx
@@ -1,25 +1,25 @@
 import { useLoaderData } from "@remix-run/react";
 import { storyblokEditable } from "@storyblok/react";
-import type { CategoryStoryblok } from "~/types";
+import type { TagStoryblok } from "~/types";
 import { PostsList } from "~/components/PostsList";
-import type { loader } from "~/routes/categories.$";
+import type { loader } from "~/routes/tags.$";
 
-export const Category = ({ blok }: CategoryStoryblok) => {
+const Tag = ({ blok }: TagStoryblok) => {
   const { story } = useLoaderData<typeof loader>();
   const filterQuery = {
-    categories: {
+    tags: {
       in_array: story.uuid,
     },
   };
-
   return (
     <div {...storyblokEditable(blok)} key={blok._uid}>
       <div className="mb-10">
         <h1>Posts for:{blok.headline}</h1>
         {blok.description ? <p>{blok.description}</p> : null}
       </div>
-
       <PostsList grid={blok.grid} filterQuery={filterQuery} />
     </div>
   );
 };
+
+export default Tag;
